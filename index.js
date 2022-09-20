64dbf49ec6ab729a3f06e58da0b5ebde27a91020
= <=>
2022-09-20 12:33:22 -0400

    refactor: add getSofiPreviousMonthTransactions to index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { chromium } from 'playwright';
-import { getSoFiBankingBalance } from './src/sofi.js';
+import { getSoFiBankingBalance, getSofiPreviousMonthTransactions } from './src/sofi.js';
 import { getWhitakerBankingBalance } from './src/whitaker.js';
 import { getWellsFargoBankingBalance } from './src/wellsfargo.js';
 import { getCashAmount } from './src/cash.js';
@@ -14,20 +14,30 @@ console.log('Starting web scraper...');
         headless: false, // Show the browser.
     });
 
-    let sofiBalance = await getSoFiBankingBalance(browser);
+    const page = await browser.newPage();
+
+    let sofiBalance = await getSoFiBankingBalance(page);
     let whitakerBalance = await getWhitakerBankingBalance(browser);
     let wellsFargoBalance = await getWellsFargoBankingBalance(browser);
     let cashAmount = await getCashAmount();
     let transamericaBalance = await getTransamericaRetirementBalance(browser);
 
+    let { 
+        transactionList,
+        foodSpeding,
+        utilitiesSpending,
+        transportationSpending,
+        miscSpending,
+        babySpending,
+        vacationSpending,
+        debtSpending,
+        healthSpending,
+        shoppingSpending,
+        income,
+    } = await getSofiPreviousMonthTransactions(page);
+
     await browser.close();
 
-    console.log(
-        `Your balances are: `,
-        sofiBalance,
-        whitakerBalance,
-        wellsFargoBalance,
-        cashAmount,
-        transamericaBalance
-    );
+    console.log('Your transactions are: ', transactionList);
+    console.log("Balances: \n", foodSpeding, utilitiesSpending, transportationSpending, miscSpending, babySpending, vacationSpending, debtSpending, healthSpending, shoppingSpending, income);
 })();
